407fa020b6b6146f98ff2d81f50bcbdcdd2444c7
Furby190 <szymonrachwalski992@gmail.com>
2022-12-09 23:38:44 +0100

    Add Footer
    feat:  Finished 'app-advertisement' -  added some SVG icons in it ( all that's left to do now, is edited two main images in this section and add on page).
    Created footer structure in HTML.
    Downloaded all SVG icons.

--- js/swiper-opinions.js
+++ js/swiper-opinions.js
@@ -4,7 +4,7 @@ const swiper = new Swiper(".opinions-container", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
-    delay: 3000,
+    delay: 2000,
     disableOnInteraction: false,
   },
   pagination: {
